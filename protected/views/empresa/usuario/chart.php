50ef08b80ce1d09cb5a7b99ede5cf7d6afd4edcb
Ruben <rbntejeda@hotmail.com>
2016-09-27 12:18:38 -0300

    Grafico usuario de cantidad de evaluaciones

--- protected/views/empresa/usuario/chart.php
+++ protected/views/empresa/usuario/chart.php
@@ -1,6 +1,93 @@
 <?php 
+# Grafico de barras - Cantidad de evaluaciones
+$query=RvFicha::CountMonthByUsuario($model->primaryKey);
+if(!empty($query)){
+$categories=array_column($query, 'CATEGORIES');
+$data=array_map('intval', array_column($query, 'DATA'));
+$this->Widget('ext.highcharts.HighchartsWidget', array(
+	'options' => array(
+		'chart'=>array(
+			'type'=>'column'
+			),
+		'title' => array('text' => 'Cantidad Total de Evaluaciones'),
+		'tooltip'=>array(
+			'valueSuffix'=>' fichas emitidas.'
+			),
+		'xAxis' => array(
+			'categories' => $categories
+			),	
+		'yAxis' => array(
+			'title' => array('text' => 'Cantidad de fichas')
+			),
+		'series' => array(	
+			array(
+				'name'=>'Total Mensual',
+				'data'=>$data
+				),
+			),
+		)
+	));
+}
+# Grafico de Cantidad de evaluaciones por usuario
+$query=array();
+$seriesName=array();
+$fechas=array();
+# Guarda fechas y nombres de las evaluacionesn junto a sus fechas
+foreach ($model->evaluaciones as $key => $value) {
+	$seriesName[]=$value->nombre;
+	$query[$key]=RvFicha::CountMonthByUsuario($model->primaryKey,$value->primaryKey);
+	$fechas=array_merge($fechas,array_column($query[$key],'CATEGORIES'));
+}
+# Elimina fecha repetidas y las ordena
+$fechas=array_unique($fechas);
+sort($fechas);
+#Recoleccion de datos por fecha
+$data=array();
+$categories=array();
+foreach ($fechas as $fecha) {
+	$categories[]=$fecha;
+	foreach ($query as $key => $eva) {
+		$flag=1;
+		foreach ($eva as $datos) {
+			if ($datos['CATEGORIES']==$fecha) {
+				$data[$key][]=intval($datos['DATA']);
+				$flag=0;
+				break;
+			}
+		}
+		if($flag){
+			$data[$key][]=0;
+		}
+	}
+}
+# Creacion de Serie
+$series=array();
+foreach ($seriesName as $key => $name) {
+	$series[]=array(
+		'name'=>$name,
+		'data'=>$data[$key]
+		);
+}
+$this->Widget('ext.highcharts.HighchartsWidget', array(
+	'options' => array(
+		'chart'=>array(
+			'type'=>'column'
+			),
+		'title' => array('text' => 'Cantidad de Evaluaciones'),
+		'tooltip'=>array(
+			'valueSuffix'=>' fichas emitidas.'
+			),
+		'xAxis' => array(
+			'categories' => $categories
+			),	
+		'yAxis' => array(
+			'title' => array('text' => 'Cantidad de fichas')
+			),
+		'series' => $series,
+		)
+	));
 
-
+# Grafico de lineas - Grafico de acierto
 $query=RvFicha::AvgByUsuario($model->primaryKey);
 $categories=array();
 $data=array();
@@ -30,7 +117,4 @@ $this->Widget('ext.highcharts.HighchartsWidget', array(
 			),
 		)
 	));
-
-
-
  ?>
